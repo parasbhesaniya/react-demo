3483e19c4a3c2239bbec3405cfdf8675858fff0f
parasbhesaniya <paras.bhesaniya@gmail.com>
2019-10-11 16:25:13 +0530

    Created component for header.

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = ({ greeting, greeting_2 }) => (
+  <div>
+    {greeting} | {greeting_2}
+  </div>
+)
+
+export default Header;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Products from './Products';
+import Header from './Header';
 
 const Home = ({ data }) => (
   <>
+    <Header greeting={data.greeting} greeting_2={data.greeting} />
     <center>
       <h2>
         All Products
-    </h2>
+      </h2>
     </center>
     <Products items={data.products} />
   </>
