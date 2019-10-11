eb2ede386955d24729493d5213f34f3fc5ece49d
parasbhesaniya <paras.bhesaniya@gmail.com>
2019-10-11 15:58:44 +0530

    Created connection to API.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Home } from "./Components";
+import { api } from './services/apiService';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    api.getData({ name: 'test' })
+      .then(res => {
+        this.setState({
+          ...res,
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  render() {
+    console.log(this.state);
+    if (this.state) {
+      return (
+        <Home />
+      );
+    }
+    return null;
+  }
 }
 
 export default App;
---
+++ src/services/apiService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+const baseUrl = ` https://mazdr9dk.herokuapp.com`
+const instance = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    'token': 'interview',
+    'Content-Type': 'application/json'
+  }
+});
+
+const api = {
+  getData: data => {
+    return instance.post('/get-mock-data', data)
+  }
+}
+
+export { api }
